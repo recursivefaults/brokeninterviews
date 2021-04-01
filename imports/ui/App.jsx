1914eb8268dbb33ae567092a81c31b3a341f5329
Ryan Latta <recursive.faults@gmail.com>
2021-04-01 13:01:49 -0500

    Scaffolding in pieces

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Nav } from './Nav.jsx';
+import { CompanyListItem } from './CompanyListItem.jsx';
 import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
 
 export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
+    <>
+    <Nav />
+    <Container>
+        <h1>Top Searches</h1>
+        <section id="search-results">
+            <ListGroup>
+                <CompanyListItem name="Toyota" grade="F" count="1000" updated="01/01/2021" />
+            </ListGroup>
+        </section>
+    </Container>
+    </>
 );
